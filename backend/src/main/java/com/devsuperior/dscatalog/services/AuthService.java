1077d99107f6a9a1add311a39ad26e45a94c898c
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-31 14:31:35 -0300

    Email does not exist

--- backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,13 +1,27 @@
 package com.devsuperior.dscatalog.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class AuthService {
 
-    public void findAllPaged(EmailDTO body) {
-        
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void createRecoverToken(EmailDTO body) {
+        User user = userRepository.findByEmail(body.getEmail());
+
+        if (user == null){
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
     }
 }
